d1c77a8edf6fe91c9349231d81ebbdbe17560a60
jijithaarun <jijitha.mj1990@gmail.com>
2020-06-15 14:02:24 +0530

    first message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CardComponent } from './card/card.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CarouselComponent,
+    AboutusComponent,
+    TestimonialsComponent,
+    CardComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
